5d7b1029cf3a0238c3986d6473680af15169db4a
Netkas <zixing.narrakas1996@gmail.com>
2022-12-23 04:46:51 -0500

    Updated test files

--- tests/parse_arguments.php
+++ tests/parse_arguments.php
@@ -1,4 +1,7 @@
 <?php
 
+    require 'ncc';
+    import('net.nosial.optslib', 'latest');
+
     $parse = \OptsLib\Parse::getArguments();
     var_dump($parse);
--- tests/parse_test.php
+++ tests/parse_test.php
@@ -1,4 +1,7 @@
 <?php
 
+    require 'ncc';
+    import('net.nosial.optslib', 'latest');
+
     $parse = \OptsLib\Parse::parseArgument('test --foo=bar --baz=qux --quux --corge --grault=garply --waldo --fred --plugh --xyzzy --thud');
     var_dump($parse);
