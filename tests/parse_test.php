c3092eb6888add742092080a014bee43d8493db3
netkas <netkas@n64.cc>
2024-09-24 00:38:54 -0400

    Removed old tests

--- tests/parse_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    require 'ncc';
-    import('net.nosial.optslib', 'latest');
-
-    $parse = \OptsLib\Parse::parseArgument('test --foo=bar --baz=qux --quux --corge --grault=garply --waldo --fred --plugh --xyzzy --thud');
-    var_dump($parse);
